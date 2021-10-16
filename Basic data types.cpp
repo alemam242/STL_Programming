2065298a48532a6c17f9ad552b97dd7d5201c9fe
Md. Al Emam <76738557+alemam242@users.noreply.github.com>
2021-10-16 13:48:29 +0600

    easy

---
+++ Basic data types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    int a;
+    long long int b;
+    char c;
+    float f;
+    double d;
+    scanf("%d %ld %c %f %lf",&a,&b,&c,&f,&d);
+    printf("%d\n%ld\n%c\n%f\n%lf\n",a,b,c,f,d);
+    return 0;
+}
